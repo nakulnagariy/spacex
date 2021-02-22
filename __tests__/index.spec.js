9727e0239a7798240e257784603c3b1d361c82b7
Nacool <nagariya23nakul@gmail.com>
2021-02-23 03:47:01 +0530

    Deleted spacex.snap

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { mount, shallow } from 'enzyme'
-import SpacexPage from '../pages/index'
+import IndexPage from '../pages/index'
 import { data } from '../mock'
 
 beforeAll(() => {
@@ -8,7 +8,6 @@ beforeAll(() => {
   //window.fetch = jest.fn(); if running browser environment
 })
 
-// src/utils/currency.test.js
 global.fetch = jest.fn(() =>
   Promise.resolve({
     json: () => Promise.resolve(data),
@@ -18,14 +17,14 @@ global.fetch = jest.fn(() =>
 describe('Pages', () => {
   describe('SpaceX', () => {
     it('should render without throwing an error', () => {
-      const wrap = mount(<SpacexPage missions={data} />)
+      const wrap = mount(<IndexPage missions={data} />)
       expect(wrap).toMatchSnapshot()
       expect(wrap.find('button').length).toBe(11)
       expect(wrap.find({ className: 'card' }).length).toBe(10)
     })
 
     it('Filter the result', async () => {
-      const wrap = mount(<SpacexPage missions={data} />)
+      const wrap = mount(<IndexPage missions={data} />)
       wrap.find('button').last().simulate('click')
       wrap.update()
       expect(wrap.props().missions.length).toBe(10)
