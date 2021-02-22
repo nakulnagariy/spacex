e0dcf0700364f8e6b593e136e4489c710378db6d
Nacool <nagariya23nakul@gmail.com>
2021-02-23 03:36:29 +0530

    Unnecessary files removed

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import Nav from './Nav'
 import Meta from './Meta'
-import Header from './Header'
 import styles from '../styles/Layout.module.css'
 
 const Layout = ({ children }) => {
